bbf9e94a8ef1738ee8f47fac19e891a0257720e7
Anusha-Rani90 <rani.anu88@gmail.com>
2021-09-06 15:55:12 +0530

    aTest commit

---
+++ pract/src/test/java/pract/pracTest1.java
@@ -0,0 +1,13 @@
+package pract;
+
+import org.testng.annotations.Test;
+
+public class pracTest1 {
+
+	@Test
+		public void test() {
+		System.out.println("hello");
+
+	}
+
+}
---
+++ practices/src/test/java/preparation/aTest.java
@@ -0,0 +1,8 @@
+package preparation;
+
+public class aTest {
+	public static void main(String[] args) {
+		System.out.println("aTest");
+	}
+
+}
